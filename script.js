57d85b5e0524ae4dc50d2d747da7e471e27e56f9
duracellB <i.schlae.h@gmail.com>
2023-02-27 17:20:41 -0500

    made the title, game, and hot to scenes

--- script.js
+++ script.js
@@ -80,176 +80,215 @@ class Player {
 const redPlayerData = new Player();
 const bluePlayerData = new Player();
 
-///////////
-/// Kaboom
-//////////
-
-////////////////////////////////
-//// background + platform ////
-
+//loading the background image
 let bgLoad = await loadSprite("background", "images/SUPERFINALBACKGROUND.png");
-let background = add([
-  sprite("background"),
-  pos(width() / 2, height() / 2),
-  origin("center"),
-  scale(), // PUTTING A PIN
-  // Allow the background to be scaled
-  // Keep the background position fixed even when the camera moves
-  fixed(),
-]);
-background.scaleTo(
-  Math.max(width() / bgLoad.tex.width, height() / bgLoad.tex.height)
-  //Math.max(width() / browserWidth, height() / browserHeight)
-);
 
-const platform = add([
-  rect(width(), 50),
-  pos(0, height()),
-  outline(4),
-  area(),
-  solid(),
-]);
-const leftPlatform = add([
-  rect(1, height()),
-  pos(0, 0),
-  outline(4),
-  area(),
-  solid(),
-]);
-const rightPlatform = add([
-  rect(1, height()),
-  pos(width(), 0),
-  outline(4),
-  area(),
-  solid(),
-]);
-
-////
-/////////////////////////
-
-/////////////////////////
-//// loading and spawning in players and logs
-
-loop(1, () => {
-  loadSprite("log", "./images/log.png");
-  add([
-    sprite("log"),
-    pos(rand(width() / 4, 3 * (width() / 4)), 0),
-    scale(0.3),
-    area(),
-    move(DOWN, 200),
-    cleanup(),
-    "log",
-  ]);
-});
-
-loadSprite("redGuy", "./images/FINALredguy.ong.png");
-const redPlayer = add([
-  sprite("redGuy"),
-  scale(0.15),
-  origin("center"),
-  area({ scale: 0.8 }),
-  pos(width() / 3, 0),
-  body(),
-  {
-    speed: 1200,
-    jumpspeed: 2500,
-  },
-]);
-
-loadSprite("blueGuy", "./images/FINALblueguyRotated.png");
-const bluePlayer = add([
-  sprite("blueGuy"),
-  scale(0.15),
-  origin("center"),
-  area({ scale: 0.8 }),
-  pos(2 * (width() / 3), 0),
-  body(),
-  {
-    speed: 1200,
-    jumpspeed: 2500,
-  },
-]);
-
-//////////////////////////
-
-//////////////////////////
-//// player movement
-
-onKeyPress("i", () => {
-  if (bluePlayer.isGrounded()) {
-    bluePlayer.jump(750);
-  }
-});
-
-onKeyDown("j", () => {
-  bluePlayer.move(-bluePlayer.speed, 0);
-});
-onKeyDown("l", () => {
-  bluePlayer.move(bluePlayer.speed, 0);
-});
-
-onKeyPress("w", () => {
-  if (redPlayer.isGrounded()) {
-    redPlayer.jump(750);
-  }
-});
-onKeyDown("a", () => {
-  redPlayer.move(-redPlayer.speed, 0);
-});
-onKeyDown("d", () => {
-  redPlayer.move(redPlayer.speed, 0);
-});
-
-////
-/////////////////////
-
-//////////////  SCORES  /////////////////
-/// creating the score numbers visually
-const redScore = add([
-  text(redPlayerData.logCount, {
-    size: 175,
-  }),
-  color(255, 0, 0),
-  pos(width() / 9, height() / 2),
-]);
-
-const blueScore = add([
-  text(bluePlayerData.logCount, {
-    size: 175,
-  }),
-  color(0, 0, 255),
-  pos(7 * (width() / 8), height() / 2),
-]);
-
-redPlayer.onCollide("log", () => {
-  redPlayerData.logCount++;
-  console.log("red player log count: ", redPlayerData.logCount);
-});
-
-bluePlayer.onCollide("log", (log) => {
-  if (bluePlayerData.logCount < 9) {
-    blueScore.text = bluePlayerData.addLog();
-    destroy(log);
-  } else if (bluePlayerData.logCount == 9) {
-    bluePlayerData.addLog();
-    destroy(log);
-    blueScore.hidden = true;
-    escape("blue");
-  }
-});
-
-// child.pos = vec2(50, 50); // set child position relative to parent
-// parent.addChild(child);
-
-///
-////////////////////////////////////////////////
-
-////////////////////  ESCAPING   ////////////////
-///conditions met for starting escape, makes the text appear
-//on the screen and stops adding logs to the players log count
+//for escaping
 let redEscape = null;
 let blueEscape = null;
+
+///////////
+/// loadings assets
+//////////
+
+loadSprite("log", "./images/log.png");
+loadSprite("redGuy", "./images/FINALredguy.ong.png");
+loadSprite("blueGuy", "./images/FINALblueguyRotated.png");
+
+//////// gamePlay scene
+scene("gamePlay", () => {
+  ///// make background
+  let background = add([
+    sprite("background"),
+    pos(width() / 2, height() / 2),
+    origin("center"),
+    scale(), // PUTTING A PIN
+    // Allow the background to be scaled
+    // Keep the background position fixed even when the camera moves
+    fixed(),
+  ]);
+  background.scaleTo(
+    Math.max(width() / bgLoad.tex.width, height() / bgLoad.tex.height)
+    //Math.max(width() / browserWidth, height() / browserHeight)
+  );
+  ////make platforms
+  const platform = add([
+    rect(width(), 50),
+    pos(0, height()),
+    outline(4),
+    area(),
+    solid(),
+  ]);
+  const leftPlatform = add([
+    rect(1, height()),
+    pos(0, 0),
+    outline(4),
+    area(),
+    solid(),
+  ]);
+  const rightPlatform = add([
+    rect(1, height()),
+    pos(width(), 0),
+    outline(4),
+    area(),
+    solid(),
+  ]);
+
+  /////////////////////////
+  //// loading and spawning in players and logs
+
+  //loadSprite("redGuy", "./images/FINALredguy.ong.png");
+  const redPlayer = add([
+    sprite("redGuy"),
+    scale(0.15),
+    origin("center"),
+    area({ scale: 0.8 }),
+    pos(width() / 3, 0),
+    body(),
+    {
+      speed: 1200,
+      jumpspeed: 2500,
+    },
+  ]);
+
+  const bluePlayer = add([
+    sprite("blueGuy"),
+    scale(0.15),
+    origin("center"),
+    area({ scale: 0.8 }),
+    pos(2 * (width() / 3), 0),
+    body(),
+    {
+      speed: 1200,
+      jumpspeed: 2500,
+    },
+  ]);
+
+  wait(2, () => {
+    loop(1.5, () => {
+      console.log("log loaded");
+      add([
+        sprite("log"),
+        pos(rand(width() / 4, 3 * (width() / 4)), 0),
+        scale(0.3),
+        area(),
+        move(DOWN, 200),
+        cleanup(),
+        "log",
+      ]);
+    });
+  });
+
+  //////////////////////////
+
+  //////////////////////////
+  //// player movement
+  onKeyPress("i", () => {
+    if (bluePlayer.isGrounded()) {
+      bluePlayer.jump(750);
+    }
+  });
+
+  onKeyDown("j", () => {
+    bluePlayer.move(-bluePlayer.speed, 0);
+  });
+  onKeyDown("l", () => {
+    bluePlayer.move(bluePlayer.speed, 0);
+  });
+
+  onKeyPress("w", () => {
+    if (redPlayer.isGrounded()) {
+      redPlayer.jump(750);
+    }
+  });
+  onKeyDown("a", () => {
+    redPlayer.move(-redPlayer.speed, 0);
+  });
+  onKeyDown("d", () => {
+    redPlayer.move(redPlayer.speed, 0);
+  });
+
+  ////
+  /////////////////////
+
+  /////////////////////////////// SCORES
+  /// creating the score numbers visually
+  const redScore = add([
+    text(redPlayerData.logCount, {
+      size: 175,
+    }),
+    color(255, 0, 0),
+    pos(width() / 9, height() / 2),
+  ]);
+
+  const blueScore = add([
+    text(bluePlayerData.logCount, {
+      size: 175,
+    }),
+    color(0, 0, 255),
+    pos(7 * (width() / 8), height() / 2),
+  ]);
+
+  redPlayer.onCollide("log", () => {
+    redPlayerData.logCount++;
+    console.log("red player log count: ", redPlayerData.logCount);
+  });
+
+  bluePlayer.onCollide("log", (log) => {
+    if (bluePlayerData.logCount < 9) {
+      blueScore.text = bluePlayerData.addLog();
+      destroy(log);
+    } else if (bluePlayerData.logCount == 9) {
+      bluePlayerData.addLog();
+      destroy(log);
+      blueScore.hidden = true;
+      escape("blue");
+    }
+  });
+
+  ////////////////////////////////////LOG STACKING
+  for (let i = 0; i < 3; i++) {
+    // pos(rand(width() / 4, 3 * (width() / 4)), 0),
+    const x = rand(width() / 4, 3 * (width() / 4));
+    const y = rand(0, height());
+
+    let log = add([
+      sprite("log"), // sprite() component makes it render as a sprite
+      pos(x, y),
+      area(), // pos() component gives it position, also enables movement        // rotate() component gives it rotation
+      origin("center"),
+      scale(0.3),
+      // origin() component defines the pivot point (defaults to "top left")
+    ]);
+
+    log.onUpdate(() => {
+      log.pos.y += 3;
+      let mainX = bluePlayer.pos.x;
+      let mainY = bluePlayer.pos.y - 30;
+      let newP;
+      if (log.isColliding(bluePlayer)) {
+        destroy(log);
+        newP = add([
+          sprite("log"), // sprite() component makes it render as a sprite
+          pos(mainX, mainY - 30),
+          area(), // pos() component gives it position, also enables movement        // rotate() component gives it rotation
+          origin("center"),
+          follow(bluePlayer, -10 * i),
+          scale(0.3),
+        ]);
+      }
+
+      if (log.pos.y > height() - 30) {
+        log.pos.y = 1;
+        log.pos.x = rand(10, width());
+      }
+    });
+  }
+});
+
+/////////////////////////////////////////////////ESCAPING
+///conditions met for starting escape, makes the text appear
+//on the screen and stops adding logs to the players log count
 function escape(player) {
   if (player == "red") {
     redEscape = add([
@@ -277,79 +316,107 @@ function escape(player) {
   }
 }
 
-////////////////////////////////////////////////
+///////////////////////////////// TITLE SCREEN
+scene("titleScreen", () => {
+  ///// make background
+  let background = add([
+    sprite("background"),
+    pos(width() / 2, height() / 2),
+    origin("center"),
+    scale(), // PUTTING A PIN
+    // Allow the background to be scaled
+    // Keep the background position fixed even when the camera moves
+    fixed(),
+  ]);
+  background.scaleTo(
+    Math.max(width() / bgLoad.tex.width, height() / bgLoad.tex.height)
+  );
 
-/////
+  //// add buttons to go to the other scenes
+  const instructButton = add([
+    rect(500, 100),
+    color(0, 0, 0),
+    area(),
+    origin("center"),
+    pos(width() / 2, 6 * (height() / 7)),
+    "howTo",
+  ]);
 
-// Create parent sprite
-// const parent = add([sprite("parent"), pos(100, 100)]);
+  add([
+    text("[How To Play].wavy", {
+      size: 60,
+      styles: {
+        wavy: (idx, ch) => ({
+          pos: vec2(0, wave(-4, 4, time() * 6 + idx * 0.5)),
+        }),
+      },
+    }),
+    area(),
+    origin("center"),
+    pos(instructButton.pos),
+    "howTo",
+  ]);
 
-// // Create child sprite
-// const child = add([
-//   sprite("child"),
-//   pos(20, 20), // set initial position relative to parent
-// ]);
+  onClick("howTo", () => {
+    go("instructions");
+  });
 
-// // Attach child to parent
-// child.pos = vec2(50, 50); // set child position relative to parent
-// parent.addChild(child);
+  add([
+    text("[Press enter to start].wavy", {
+      size: 60,
+      styles: {
+        wavy: (idx, ch) => ({
+          pos: vec2(0, wave(-4, 4, time() * 6 + idx * 0.5)),
+        }),
+      }
+    }),
+    origin("center"),
+    pos(width() / 2, height() / 7),
+    color(255, 255, 255),
+  ]);
 
-// child.pos = vec2(50, 50); // set child position relative to parent
-// child.parent = parent; // set parent of child sprite
-// >>>>>>> 347e28c07663fb53d49cdca794df5b1d138c2e96
+  onKeyRelease("enter", () => {
+    go("gamePlay");
+  });
 
+});
 
-for (let i = 0; i < 3; i++) {
+///////////////////////////////// INSTRUCTIONS SCREEN
+scene("instructions", () => {
+  let background = add([
+    sprite("background"),
+    pos(width() / 2, height() / 2),
+    origin("center"),
+    scale(), // PUTTING A PIN
+    // Allow the background to be scaled
+    // Keep the background position fixed even when the camera moves
+    fixed(),
+  ]);
+  background.scaleTo(
+    Math.max(width() / bgLoad.tex.width, height() / bgLoad.tex.height)
+  );
 
-  // pos(rand(width() / 4, 3 * (width() / 4)), 0),
-  const x = rand(width() / 4, 3 * (width() / 4))
-  const y = rand(0, height())
+  add([
+    text("[Press enter to start].wavy", {
+      size: 60,
+      styles: {
+        wavy: (idx, ch) => ({
+          pos: vec2(0, wave(-4, 4, time() * 6 + idx * 0.5)),
+        }),
+      },
+    }),
+    origin("center"),
+    pos(width() / 2, height() / 7),
+    color(255, 255, 255),
+  ]);
 
-  let log = add([
-      sprite("log"),   // sprite() component makes it render as a sprite
-      pos(x, y), 
-      area(),// pos() component gives it position, also enables movement        // rotate() component gives it rotation
-      origin("center"),
-      scale(0.3)
-  // origin() component defines the pivot point (defaults to "topleft")
-    ])
-  
-  log.onUpdate(() => {
-    log.pos.y += 3
-    let mainX = bluePlayer.pos.x
-    let mainY = bluePlayer.pos.y - 30
-    let newP;
-    if (log.isColliding(bluePlayer)){
-      destroy(log)
-      newP = add([
-      sprite("log"),   // sprite() component makes it render as a sprite
-      pos(mainX,mainY -30), 
-      area(),// pos() component gives it position, also enables movement        // rotate() component gives it rotation
-      origin("center"),
-      follow(bluePlayer, -10* i ),
-      scale(0.3)
-    ])	
-      
-    }
+  onKeyRelease("enter", () => {
+    go("gamePlay");
+  });
+});
 
-    
-    if(log.pos.y > height() -30){
-      log.pos.y = 1
-      log.pos.x = rand(10, width())
-    }
-
-  })
-
-
+/////////////////////////////////// STARTING THE GAME
+function startGame() {
+  go("titleScreen");
 }
-
-
-
-////// redGuy
-
-
-
-
-
-
-
+startGame();
